d279686a236a1117e949950dac0676aede36a77b
Eduardo Bruns <eduardo.bruns@gmail.com>
2022-07-02 15:38:58 -0300

    URL Slug

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,13 @@
 class Room < ApplicationRecord
+  extend FriendlyId
+
   has_many :reviews, dependent: :destroy
   belongs_to :user
 
   scope :most_recent, -> { order('created_at DESC') }
+
+  validates_presence_of :title, :slug
+  friendly_id :title, use: [:slugged, :history, :finders]
   
   def complete_name
     "#{title}, #{location}"
